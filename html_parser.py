750756de059837dae2ee49191b7e84646149955a
liuqiuchen <2016968116@qq.com>
2016-10-06 11:15:11 +0800

    edit and add codes

--- html_parser.py
+++ html_parser.py
@@ -1,13 +1,26 @@
 # coding=utf8
+from bs4 import BeautifulSoup
 
 
 class HtmlParser(object):
     
     
-    def parse(self):
+    def _get_new_urls(self, page_url, soup):
         pass
     
     
+    def _get_new_data(self, page_url, soup):
+        pass
+    
+    
+    def parse(self, page_url, html_cont):
+        if page_url is None or html_cont is None:
+            return
+    
+        soup = BeautifulSoup(html_cont, 'html_parser', from_encoding='utf-8')
+        new_urls = self._get_new_urls(page_url, soup)
+        new_data = self._get_new_data(page_url, soup)
+        return new_urls, new_data
 
 
 
