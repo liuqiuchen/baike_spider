6e0f6b844e783ecd8bd8081e64240b1d3aced048
liuqiuchen <2016968116@qq.com>
2016-10-07 13:55:04 +0800

    edit

--- baike_spider2/main.py
+++ baike_spider2/main.py
@@ -24,7 +24,7 @@ def html_parser(url, html_cont):
     
     return datas
 
-def out_html(url, data):
+def out_html(root_url, root_data, datas):
     fout = open('output.html', 'w')
     
     fout.write("<html>")
@@ -34,9 +34,18 @@ def out_html(url, data):
     fout.write("<body>")
     fout.write("<table>")
     
-    fout.write("<td>%s</td>" % data['title'].encode('utf-8')) 
-    fout.write("<td>%s</td>" % data['url'].encode('utf-8'))
-    fout.write("<td>%s</td>" % data['summary'].encode('utf-8'))
+    fout.write("<tr>")
+    fout.write("<td>%s</td>" % root_data['title'].encode('utf-8')) 
+    fout.write("<td>%s</td>" % root_url.encode('utf-8'))
+    fout.write("<td>%s</td>" % root_data['summary'].encode('utf-8'))
+    fout.write("</tr>")
+    
+    for data in datas:
+        fout.write("<tr>")
+        fout.write("<td>%s</td>" % data['title'].encode('utf-8')) 
+        fout.write("<td>%s</td>" % data['url'].encode('utf-8'))
+        fout.write("<td>%s</td>" % data['summary'].encode('utf-8'))
+        fout.write("</tr>")
         
     fout.write("</table>")
     fout.write("</body>")
@@ -45,31 +54,60 @@ def out_html(url, data):
     fout.close()
     
 def get_new_urls(html_cont):
-    soup = BeautifulSoup(html_cont, 'html.parser', from_encoding='utf-8')
-    links = soup.find_all('a', href=re.compile(r"/view/\d+\.htm"))
-    new_urls = set()
-    new_url_list = []
-    count = 0;
-    for link in links:
-        count = count + 1
+    try:
+        soup = BeautifulSoup(html_cont, 'html.parser', from_encoding='utf-8')
+        links = soup.find_all('a', href=re.compile(r"/view/\d+\.htm"))
+        new_urls = set()
+        new_url_list = []
+        count = 0;
+        if len(links) >= 10: 
+            for link in links:
+                
+                if count < 10: # 抓取10个url上的数据
+                    new_url = link['href']
+                    new_full_url = urlparse.urljoin('http://baike.baidu.com', new_url)
+                    new_urls.add(new_full_url)
+                    new_url_list.append(new_urls.pop())
+                    
+                count = count + 1 
+        else:
+            print '数据不足10条'
+       
+        # print list(set(new_url_list)) # 列表去重
         
-        if count < 10:
-            new_url = link['href']
-            new_full_url = urlparse.urljoin('http://baike.baidu.com', new_url)
-            new_urls.add(new_full_url)
-            new_url_list.append(new_urls.pop()) 
-   
-    print list(set(new_url_list)) # 列表去重
+        return list(set(new_url_list)) 
     
-    return list(set(new_url_list))    
+    except:
+        print '爬取失败'
+        return None
+
+def get_datas(urls):   
+    all_datas = []
+    # count = 0;
+    print '开始爬取...'
+    for link in urls:
+        # print link
+        html_cont = downloader(link)
+        all_datas.append(html_parser(link, html_cont))
+        # print all_datas[count]
+        # count = count + 1
+        
+    print '数据获取完毕！'
+        
+    return all_datas
 
 # 下载网页，入口网址
 root_url = "http://baike.baidu.com/view/21087.htm"
 
+# 下载入口url
 html_cont = downloader(root_url)
-datas = html_parser(root_url, html_cont)
-out_html(root_url, datas)
-
-get_new_urls(html_cont)
+# 解析入口url数据
+root_data = html_parser(root_url, html_cont)
+# 得到入口url里所有的url
+urls = get_new_urls(html_cont)
+# 得到符合条件的数据
+datas = get_datas(urls)
+# 输出数据
+out_html(root_url, root_data, datas)
 
 
