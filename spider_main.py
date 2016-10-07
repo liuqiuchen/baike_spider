8fc3629d1505647c3c04cb81d868757dcad7e2aa
liuqiuchen <2016968116@qq.com>
2016-10-07 13:05:22 +0800

    add

--- spider_main.py
+++ spider_main.py
@@ -1,10 +1,11 @@
 # coding:utf8
-from baike_spider import url_manager, html_downloader, html_parser,\
-    html_outputer
+
+from baike_spider import url_manager, html_downloader, html_parser, html_outputer
 
 class SpiderMain(object):
     # 构造函数
     def __init__(self):
+        print 'init'
         self.urls = url_manager.UrlManager()
         self.downloader = html_downloader.HtmlDownloader()
         self.parser = html_parser.HtmlParser()
@@ -13,10 +14,11 @@ class SpiderMain(object):
     def craw(self, root_url):
         count = 1
         self.urls.add_new_url(root_url)
+        # print self.urls.has_new_url()
         while self.urls.has_new_url():
             try:
                 new_url = self.urls.get_new_url()
-                print 'craw %d : %s' % (count, new_url)
+                print "craw %d : %s" % (count, new_url)
                 html_cont = self.downloader.download(new_url)
                 new_urls, new_data = self.parser.parse(new_url, html_cont)
                 self.urls.add_new_urls(new_urls)
